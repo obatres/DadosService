88c895491f478565b81ddb6800d755c4e986da2e
Orlando <batres.93@gmail.com>
2020-10-13 22:03:16 -0600

    Retorno de numero de dados

--- Dados/Service.py
+++ Dados/Service.py
@@ -1,14 +1,25 @@
 from flask import Flask, request, json, Response
+from random import randint as ran 
 import json
 
 app = Flask(__name__)
 
+def retornarTiros(cantidad):
+    tiros=[]
+    for i in range(cantidad):
+        tiros.append(ran(1,6))
+    return tiros
 
-
-@app.route('/cantidad', methods=['GET'])
-def index():
-    res = {'Status': 'Successfully ' + 'request ok'}
-    return Response(response=json.dumps(res),
+@app.route('/tirar/<int:cantidad>', methods=['GET'])
+def index(cantidad):
+    if(type(cantidad)!=int or cantidad<=0):
+        return Response(response="Número de dados a tirar no es válido",
+                    status=400,
+                    mimetype='application/json')
+    else:
+        tiros = retornarTiros(cantidad)
+        res = {'dados': tiros}
+        return Response(response=json.dumps(res),
                     status=200,
                     mimetype='application/json')
 
